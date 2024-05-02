5707baccee018ac6acd7698fc6f933c53c438072
Davidhernandezalt <146228222+Davidhernandezalt@users.noreply.github.com>
2024-05-02 12:22:11 -0600

    test
    test widget

---
+++ widget.js
@@ -0,0 +1,12 @@
+// Definición de la función para inicializar el widget
+function initWidget(containerId) {
+    // Obtén el contenedor donde se insertará el widget
+    var container = document.getElementById(containerId);
+
+    // Crea el contenido del widget
+    var widgetContent = document.createElement("div");
+    widgetContent.innerHTML = "<h2>Este es mi widget</h2><button onclick='alert(\"Hola desde el widget!\")'>Haz clic</button>";
+
+    // Agrega el contenido del widget al contenedor
+    container.appendChild(widgetContent);
+}
